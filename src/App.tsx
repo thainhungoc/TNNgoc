e0a7173a55b94f691da0546682f270cda9726d73
Yuamy205 <ngocarmy0710g@gmail.com>
2024-06-20 22:54:59 +0700

    commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BT } from "./components/ss40/BT"
+import './components/ss40/Baitap/index.css'
+
+
+
+
+
+function App() {
+  return (
+    <>
+    
+    <BT/>
+
+    
+    </>
+  )
+}
+
+export default App
